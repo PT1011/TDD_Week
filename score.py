f55312035f2f8b23243d2fb8ac38d07fa43be344
Parth Thite <parthpthite@gmail.com>
2026-03-26 08:05:28 -0400

    Update score.py

--- score.py
+++ score.py
@@ -1,7 +1,7 @@
 def add_points(game, amount):
     if amount < 0:
         raise ValueError("Please enter a positive number")
-    elif game["active"] == False:
+    elif not game["active"]:
         return game
     else:
         game["score"] += (amount * game["multiplier"])
@@ -11,7 +11,7 @@ def add_points(game, amount):
 def apply_multiplier(game, multiplier):
     if multiplier < 1:
         raise ValueError("Multiplier must be greater than or equal to 1")
-    elif game["active"] == False:
+    elif not game["active"]:
         return game
     else:
         game["multiplier"] = multiplier
@@ -31,4 +31,4 @@ def is_high_score(game, threshold):
         return True
     else:
         return False
-
+        
